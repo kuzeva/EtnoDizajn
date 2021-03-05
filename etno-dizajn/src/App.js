f26491a195d4a8207b50d06d448d23b709d7bbbb
BojanaKuzeva <you@example.com>
2021-03-05 15:43:53 +0100

    initial

---
+++ etno-dizajn/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+};
+
+export default App;
